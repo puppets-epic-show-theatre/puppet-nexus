3dccd62a12bc3af0deda4d6579d917b694fb8103
Stephan Eicher <stephan.eicher@check24.de>
2024-06-21 13:56:30 +0200

    Update to latest pdk template (#63)

--- manifests/resource/repository/npm/proxy.pp
+++ manifests/resource/repository/npm/proxy.pp
@@ -65,7 +65,7 @@ define nexus::resource::repository::npm::proxy (
         'strictContentTypeValidation' => $storage_strict_content_type_validation,
         'writePolicy'                 => $storage_write_policy,
       },
-      'cleanup'   => {
+      'cleanup'         => {
         'policyNames' => $cleanup_policy_names,
       },
       'proxy'           => {
