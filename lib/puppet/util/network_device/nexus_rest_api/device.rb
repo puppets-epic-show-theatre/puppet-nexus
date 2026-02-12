4610cb581d82940f382c61dcf639aa5cac61fb76
Stephan Eicher <stephan.eicher@check24.de>
2026-02-12 11:19:06 +0100

    Fix broken tests and linting issues (#87)

--- lib/puppet/util/network_device/nexus_rest_api/device.rb
+++ lib/puppet/util/network_device/nexus_rest_api/device.rb
@@ -4,11 +4,11 @@ require 'puppet/resource_api/transport/wrapper'
 
 # Initialize the NetworkDevice class if necessary
 class Puppet::Util::NetworkDevice; end
-class Puppet::Util::NetworkDevice::Nexus_rest_api; end
+class Puppet::Util::NetworkDevice::NexusRestApi; end
 
-# The Nexus_rest_api module only contains the Device class to bridge from puppet's internals to the Transport.
+# The NexusRestApi module only contains the Device class to bridge from puppet's internals to the Transport.
 # All the heavy lifting is done bye the Puppet::ResourceApi::Transport::Wrapper
-class Puppet::Util::NetworkDevice::Nexus_rest_api::Device < Puppet::ResourceApi::Transport::Wrapper # rubocop:disable Style/ClassAndModuleCamelCase
+class Puppet::Util::NetworkDevice::NexusRestApi::Device < Puppet::ResourceApi::Transport::Wrapper
   # Bridging from puppet to the nexus_rest_api transport
   def initialize(url_or_config, _options = {})
     super('nexus_rest_api', url_or_config)
